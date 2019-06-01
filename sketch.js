6e7981504de9c0c5e4a034efb90a23d514562541
Carbon <robotcreeper13@gmail.com>
2019-06-01 17:52:02 -0500

    fistcat fix

--- sketch.js
+++ sketch.js
@@ -35,7 +35,7 @@ function setup() {
     players[data.sid].x = data.x;
     players[data.sid].y = data.y;
   });
-  fistCat.resize(30,30)
+  //fistCat.resize(30,30)
   player = new Player(100,100)
 }
 
