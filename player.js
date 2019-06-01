d1b8ce325080db43bc845abfca03ea7f91973c13
Jack <yoshifan509@gmail.com>
2019-06-01 16:15:56 -0500

    added classes

--- player.js
+++ player.js
@@ -1,10 +1,53 @@
-class Player {
-    constructor() {
+class Player { // Fists
+    constructor(x,y) {
         this.health = 100
         this.shield = 0
         this.speed = 5 // divide by 4th
         this.level = 0
         this.xp = 0
         this.points = 0
+        this.strength = 5
+        this.x = x
+        this.y = y
+    }
+}
+
+class Engineer extends Person {
+    constructor(x,y){
+        super()
+        this.shield = 25
+        this.speed = 4.75
+        this.level = 1
+        this.strength = 8
+    }
+}
+
+class Blade extends Person {
+    constructor(x,y){
+        super()
+        this.shield = 50
+        this.speed = 4.75
+        this.level = 1
+        this.strength = 10
+    }
+}
+
+class Mage extends Person {
+    constructor(x,y) {
+        super()
+        this.shield = 15
+        this.speed = 4.75
+        this.level = 1
+        this.strength = 10
+    }
+}
+
+class Bow extends Person {
+    constructor(x,y) {
+        super()
+        this.shield = 15
+        this.speed = 4.75
+        this.level = 2
+        this.strength = 10
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,28 +1,52 @@
+let player;
+
 function preload(){
-  // put preload code here
+  fistCat = loadImage('assets/img/fistCat.png')
+  // engiCat = loadImage('assets/img/engiCat.png')
+  // mageCat = loadImage('assets/img/mageCat.png')
+  // bowCat = loadImage('assets/img/bowCat.png')
 }
 
 function setup() {
   createCanvas(800, 800)
+  fistCat.resize(30,30)
+  player = new Player(100,100)
 }
 
 function draw() {
   background(0);
-  
+  image(fistCat, player.x, player.y)
+  if (keyIsDown(65)) { //A
+    console.log(`Key ${keyCode.key} pressed.`)
+    player.x -= player.speed
+  } 
+  if (keyIsDown(68)) {//D
+    console.log(`Key ${keyCode.key} pressed.`)
+    player.x += player.speed
+  }  
+  if (keyIsDown(87)) { //W
+    console.log(`Key ${keyCode.key} pressed.`)
+    player.y -= player.speed
+  }  
+  if (keyIsDown(83)) {//S
+    console.log(`Key ${keyCode.key} pressed.`)
+    player.y += player.speed
+  } 
+
 }
 
 function keyPressed(keyCode) {
-  if (keyCode.keyCode == 65) { //A
+  if (keyCode.keyCode == 65 && keyIsDown(keyCode.keyCode)) { //A
     console.log(`Key ${keyCode.key} pressed.`)
-    value = 255;
+    player.x -= player.speed
   } else if (keyCode.keyCode == 68) {//D
     console.log(`Key ${keyCode.key} pressed.`)
-    value = -255;
+    player.x += player.speed
   } else if (keyCode.keyCode == 87) { //W
     console.log(`Key ${keyCode.key} pressed.`)
-    value = 255;
+    player.y -= player.speed
   } else if (keyCode.keyCode == 83) {//S
     console.log(`Key ${keyCode.key} pressed.`)
-    value = 255;
+    player.y += player.speed
   }
 }
