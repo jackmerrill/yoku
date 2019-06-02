f73e0b6204da899afd7d8d8d493d8bd667765275
Carbon <robotcreeper13@gmail.com>
2019-06-02 01:27:19 -0500

    Added shit

--- assets/tilemap/Tileset_1.tsx
+++ assets/tilemap/Tileset_1.tsx
@@ -1,13 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="Tileset 1" tilewidth="900" tileheight="900" tilecount="3" columns="0">
  <grid orientation="isometric" width="1" height="1"/>
+ <terraintypes>
+  <terrain name="grass3" tile="0"/>
+  <terrain name="grass2" tile="1"/>
+  <terrain name="grass1" tile="2"/>
+ </terraintypes>
  <tile id="0">
   <image width="900" height="900" source="grassj1.jpg"/>
  </tile>
  <tile id="1">
   <image width="900" height="900" source="grassj2.jpg"/>
  </tile>
- <tile id="2">
+ <tile id="2" terrain=",,,1">
   <image width="900" height="900" source="grassj3.jpg"/>
  </tile>
 </tileset>
--- sketch.js
+++ sketch.js
@@ -3,12 +3,15 @@ let socket;
 let SID;
 let players = {};
 let playersNew = {};
+let x= 76500
+let y = 4800
 function preload(){
   fistB = loadImage('assets/img/fistCat/fistB.png')
   fistF = loadImage('assets/img/fistCat/fistF.png')
   fistL = loadImage('assets/img/fistCat/fistL.png')
   fistR = loadImage('assets/img/fistCat/fistR.png')
   fistH = loadImage('assets/img/fistCat/fistH.png')
+  tmap = loadTiledMap("Yoku", "data");
   // engiCat = loadImage('assets/img/engiCat.png')
   // mageCat = loadImage('assets/img/mageCat.png')
   // bowCat = loadImage('assets/img/bowCat.png')
@@ -27,7 +30,7 @@ function setup() {
   socket.on('connect', function(sock){
     socket.emit('initPlayer',player)
     print("connected")
-
+  
   });
   socket.on('initPlayer',function() {
     print("emit")
@@ -43,17 +46,24 @@ function setup() {
 
   socket.on('player move',function(data){
     players[data.sid].sid = data.sid;
-    players[data.sid].x = lerp(players[data.sid].x, data.x, 0.05)
-    players[data.sid].y = lerp(players[data.sid].y, data.y, 0.05)
+    players[data.sid].x = data.x
+    players[data.sid].y = data.y
     players[data.sid].direction = data.direction;
     players[data.sid].cass = data.cass;
   });
-  player = new Player(100,100,"name")
-  
+  player = new Player(width/2,width/2,"name")
+  s = tmap.getMapSize();
+  fill(0)
 }
 
 function draw() {
   background(0);
+  //background(tmap.getBackgroundColor());
+  tmap.draw(x, y);
+  fill(255)
+  text("Camera Coords: "+x+","+y,10,255)
+  text("Map Size: " + tmap.getMapSize(), 10, 300);
+  text("Layer 0 Type: " + tmap.getType(0), 10, 350);
   //image(fistCat, player.x, player.y)
   for (let i = 0; i < Object.keys(players).length; i++) {
     pyer = players[Object.keys(players)[i]];
@@ -92,6 +102,12 @@ function draw() {
   if(needsupdate){
     socket.emit('move',player)
   }
+  if (keyIsPressed) {
+    if(key == 'a' || key == 'A') x -= player.speed;
+    if(key == 'd' || key == 'D') x += player.speed;
+    if(key == 'w' || key == 'W') y -= player.speed;
+    if(key == 's' || key == 'S') y += player.speed;
+}
   
 }
 
